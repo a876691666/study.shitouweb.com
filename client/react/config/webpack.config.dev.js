6a1e1e539edf3f1788e4be46d1f6d06812ed3a37
a876691666 <928200728@qq.com>
2018-05-13 11:20:49 +0800

    增加react代码

---
+++ client/react/config/webpack.config.dev.js
@@ -0,0 +1,87 @@
+const 
+  webpack = require('webpack'),
+  path = require('path'),
+  glob = require('glob');
+
+
+var 
+  ExtractTextPlugin = require("extract-text-webpack-plugin"),
+  HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+
+
+
+/***********************设置module.exports中的plugins***************************/
+/*
+    定义一个数组，module.exports中的plugins项可以直接使用这个数组
+ */
+var plugins = []; 
+/*
+    调用ExtractTextPlugin，把单独的css打到dist/css/下面，该路径也是从distPath开始
+    [name]为引用这个css文件的js文件的入口文件打包后的名字，即入口文件output后的名字
+ */
+plugins.push(new ExtractTextPlugin("css/[name].css")); 
+plugins.push(new HtmlWebpackPlugin({
+  filename:'../index.html',
+  template: 'src/index.html'
+}))
+
+
+
+module.exports = {
+  optimization:{
+    splitChunks:{
+      name:'common'
+    },
+    minimize:true
+  },
+  entry: path.resolve(__dirname, '../src/index.js'), 
+  output: {
+      path: path.resolve(__dirname, '../dist'), // 输出的路径
+      publicPath: '/react/dist/',
+      filename: 'bundle.js'  // 打包后文件
+  },
+  mode:'development',
+  resolve: {
+      //require文件的时候不需要写后缀了，可以自动补全
+      extensions: ['*', '.js', '.jsx','.css']
+  },
+  plugins:plugins,
+  module: {
+    rules: [
+      {
+        test:/\.jsx?$/,
+        exclude:/node_modules/,
+        loader:'babel-loader',
+        query:{
+          presets:['react','es2015']
+        }
+      },
+      {
+          test: /\.html$/,
+          use: [
+            {loader: "html-loader"}
+          ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          { loader: 'file-loader' },
+        ]
+      }
+    ]
+  }
+}
---
+++ client/react/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import app from './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={app.App}>
+        <header className={app.AppHeader}>
+          <img src={logo} className={app.AppLogo} alt="logo" />
+          <h1 className={app.AppTitle}>Welcome to React</h1>
+        </header>
+        <p className={app.AppIntro}>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
