9dca0204087c8b53e157a484f1657a4fdc276843
a876691666 <928200728@qq.com>
2019-05-28 22:20:29 +0800

    Fix: 解决index页面显示源码的问题

--- server/fe-menu-server/index.js
+++ server/fe-menu-server/index.js
@@ -3,6 +3,11 @@ var router = express.Router();
 const { feMenu } = require("../mysql");
 
 router.get("/javascriptApi/getGlobalList", function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By", " 3.2.1");
+  res.header("Content-Type", "application/json;charset=utf-8");
   feMenu.get("javascriptApiGlobalList", "apiName, description").then(dbRes => {
     res.send(dbRes.data);
   });
